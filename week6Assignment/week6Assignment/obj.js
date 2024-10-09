2ee3a29e787f45c795d98eca76e64f7c26857319
mskwashie <ikwashie@uwyo.edu>
2024-10-09 15:52:32 -0600

    f

---
+++ week6Assignment/week6Assignment/obj.js
@@ -0,0 +1,15 @@
+var myStuff = {
+    "Graduate School": {city: "University of Wyoming", state: "Wyoming", "coord": [-105.502, 41.3104] },
+    "Undergrad": {city: "Kumasi", state: "Ashanti Region", "coord": [-1.5904761, 6.6830685]},
+    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.502, 41.3104]},
+    "Favorite City": {city: "Brooklyn", state: "New York", "coord": [-73.949997, 40.650002] },
+    "Place of Birth": {city: "Koforidua", state: "Eastern Region", "coord": [-0.250000, 6.083333]},
+    "Middle School": {city: "Wesley International School", state: "Eastern Region", "coord": [-0.3412888, 6.0702097]},
+    "Town with Serene places": {city: "Sogakope", state: "Volta Region", "coord": [0.535898, 5.9662552]},
+    "Favorite Beach": {city: "Takoradi", state: "Western Region", "coord": [-1.8487208, 4.8762929]},
+    "Favorite Icecream Shop": {city: "Pinochio", state: "Greater Accra Region", "coord": [-0.232214, 5.6370478]},
+    "Favorite Chinese Food Restaurant": {city: "Noble House Chinese Restaurant", state: "Greater Accra Region", "coord": [-138.8914831, 1.722984]},
+    "Place to Visit": {city: "Eiffel Tower", state: "France", "coord": [2.294694, 48.858093]}
+}
+
+
