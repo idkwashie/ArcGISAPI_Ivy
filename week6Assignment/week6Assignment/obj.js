ceeecc5f28d82109d22b7ee60e022eb7884579a3
mskwashie <ikwashie@uwyo.edu>
2024-10-09 15:04:18 -0600

    fixing a display problem

--- week6Assignment/week6Assignment/obj.js
+++
@@ -1,8 +0,0 @@
-var myStuff = {
-    "Graduate School": {city: "University of Wyoming", state: "Wyoming", "coord": [-105.502, 41.3104] },
-    "Undergrad": {city: "Kumasi", state: "Ashanti Region", "coord": [-1.5904761, 6.6830685]},
-    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.502, 41.3104]},
-    "Favorite City": {city: "Brooklyn", state: "New York", "coord": [-73.949997, 40.650002] }
-}
-
-
